05efd3c1243bc68e0f1cadf6d32cb2ff25e62a8d
Raymond Julin <raymond@devel.apps8.hardware.no.webdealhosting.com>
2009-08-11 20:52:59 +0200

    Added a demo page with documentation

---
+++ page.js
@@ -0,0 +1,36 @@
+$(function(){
+    $("form select,form input").autosave({
+        grouped:false,
+        success:function(data) {
+            if ("name" in data)
+                $("<span>"+data.name+"</span><br/>").appendTo($("#demodebug"));
+            if ("text" in data)
+                $("<span>"+data.text+"</span><br/>").appendTo($("#demodebug"));
+        }
+    });
+    // Set up documentation shit
+    // Ruthlessly copied from multiselect
+    $("#optionTabs").tabs();
+    $('.options-list').find('.option-description, .option-examples').hide().end()
+        .find('.option-name a').click(function() {
+            var a = $(this);
+            var p = a.parents(a.attr('href'));
+            p.find('.option-examples, .option-description')['slide' + (p.find('.option-description').is(':visible') ? 'Up' : 'Down')]('fast');
+            return false;
+        });
+        $('.methods-list').find('.method-header > dl, .method-description').hide().end()
+        .find('.method-name a').click(function() {
+            var a = $(this);
+            var p = a.parents(a.attr('href'));
+            p.find('.method-header > dl, .method-description')['slide' + (p.find('.method-description').is(':visible') ? 'Up' : 'Down')]('fast');
+            return false;
+        });
+        $('.events-list').find('.event-description, .event-examples').hide().end()
+        .find('.event-name a').click(function() {
+            var a = $(this);
+            var p = a.parents(a.attr('href'));
+            p.find('.event-examples, .event-description')['slide' + (p.find('.event-description').is(':visible') ? 'Up' : 'Down')]('fast');
+            return false;
+        });
+
+});
