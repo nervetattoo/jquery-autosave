a77edcc19718c0114e2a6061ea88c0b0290c3a9b
Kyle Florence <kyle.florence@gmail.com>
2011-01-11 11:45:30 -0800

    getting there

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -1,174 +1,178 @@
 /**
- * jQuery Plugin Autosave
+ * jQuery.autosave
  *
  * @author Raymond Julin (raymond[dot]julin[at]gmail[dot]com)
  * @author Mads Erik Forberg (mads[at]hardware[dot]no)
  * @author Simen Graaten (simen[at]hardware[dot]no)
  *
  * Licensed under the MIT License
- *
- * Usage: 
- * $("input.autosave").autosave({ 
- *     url: url, // Defaults to parent form url or window.location.href
- *     method: "post",  // Defaults to parent form url or get
- *     grouped: true, // Defaults to false. States whether all selected fields should be sent in the request or only the one it was triggered upon
- *     success: function(data) { 
- *         console.log(data); 
- *     },
- *     send: function(eventTriggeredByNode) { 
- *         // Do stuff while we wait for the ajax response, defaults to doing nothing
- *         console.log("Saving");
- *     },
- *     error: function(xmlReq, text, errorThrown) { 
- *         // Handler if the ajax request fails, defaults to console.log-ing the ajax request scope
- *         console.log(text);
- *     },
- *     dataType: "json" // Defaults to JSON, but can be XML, HTML and so on
- * });
- *
- * $("form#myForm").autosave(); // Submits entire form each time one of the 
- *                              // elements are changed, except buttons and submits
- *
- *
- * Todo:
- * - Support timed autosave for textareas
  */
 
 (function($) {
-    $.fn.autosave = function(options) {
-        /**
-         * Define some needed variables
-         * elems is a shortcut for the selected nodes
-         * nodes is another shortcut for elems later (wtf)
-         * eventName will be used to set what event to connect to
-         */
-        var elems = $(this), nodes = $(this), eventName;
+  $.fn.autosave = function(options) {
+    options = $.extend(true, $.fn.autosave.options, options);
 
-        options = $.extend({
-            grouped: false,
-            send: false, // Callback
-            error: false, // Callback
-            success: false, // Callback
-            dataType: "json" // From ajax return point
-        }, options);
-        
-        /**
-         * If the root form is used as selector
-         * bind to its submit and find all its
-         * input fields and bind to them
-         */
-        if ($(this).is('form')) {
-            /* Group all inputelements in this form */
-            options.grouped = true;
-            elems = nodes = $(this).find(":input,button");
-            // Bind to forms submit
-            $(this).bind('submit', function(e) {
-                e.preventDefault();
-                $.fn.autosave._makeRequest(e, nodes, options, $(this));
-            });
-        }
-        /**
-         * For each element selected (typically a list of form elements
-         * that may, or may not, reside in the same form
-         * Build a list of these nodes and bind them to some
-         * onchange/onblur events for submitting
-         */
-        elems.each(function(i) {
-            eventName = $(this).is('button,:submit') ? 'click' : 'change';
-            $(this).bind(eventName, function (e) {
-                eventName == 'click' ? e.preventDefault() : false;
-                $.fn.autosave._makeRequest(e, nodes, options, this);
-            });
+    var timer,
+      changed = [],
+      $elements = this,
+      rprops = /^(checked|selectedIndex)$/,
+      interval = !isNaN(parseInt(options.save.onInterval))
+        && options.save.onInterval > 0 ? save.onInterval : 5000,
+      eventType = options.save.onEvent ? (typeof options.save.onEvent
+        === "string" ? options.save.onEvent : "autosave") : false,
+      condition = typeof options.save.onCondition === "function"
+        ? options.save.onCondition : false,
+      // returns the forms associated with targets
+      forms = function(targets) {
+        var $forms = targets ? $(targets) : $elements;
+
+        // we only want unique form instances, no duplicates
+        return $($.unique($forms.map(function() {
+          return this.elements ? this : this.form;
+        }).get()));
+      },
+      // returns the form fields associated with targets
+      fields = function(targets) {
+        var $fields = targets ? $(targets) : $elements;
+
+        // extracts form fields if a form element is found
+        return $fields.map(function() {
+          return this.elements ? $.makeArray(this.elements) : this;
         });
-        return $(this);
-    }
-    
-    /**
-     * Actually make the http request
-     * using previously supplied data
-     */
-    $.fn.autosave._makeRequest = function(e, nodes, options, actsOn) {
-        // Keep variables from global scope
-        var vals = {}, form;
-        /**
-         * Further set default options that require
-         * to actually inspect what node autosave was triggered upon
-         * Defaults:
-         *  -method: post
-         *  -url: Will default to parent form if one is found,
-         *        if not it will use the current location
-         */
-        form = $(actsOn).is('form') ? $(actsOn) : $(actsOn.form);
-        options = $.extend({
-            url: (form.attr('action'))? form.attr('action') : window.location.href,
-            method: (form.attr('method')) ? form.attr('method') : "post"
-        }, options);
+      },
+      // setTimeout loop for saving on intervals
+      loop = function(targets) {
+        // using setTimeout ensures our requests are processed in the
+        // order they are received but may delay them slightly
+        if (targets !== false) {
+          console.log("loop:", targets);
+          timer = setTimeout(function() {
+            notify(targets, timer);
+          }, interval);
+        } else {
+          clearTimeout(timer);
+          timer = undefined;
+        }
+      },
+      // called whenever there is new data to be saved, or on an interval
+      notify = function(targets, context) {
+        console.log("notify:", targets);
 
-        /**
-         * If options.grouped is true we collect every
-         * value from every node
-         * But if its false we should only push
-         * the one element we are acting on
-         */
-        if (options.grouped) {
-            nodes.each(function (i) {
-                /**
-                 * Do not include button and input:submit as nodes to 
-                 * send, EXCEPT if the button/submit was the explicit
-                 * target, aka it was clicked
-                 */
-                if (!$(this).is('button,:submit') || e.currentTarget == this) {
-                    if ($(this).is(':radio') && $(this).attr('checked')==false)
-                        return;
-                    vals[this.name] = $(this).is(':checkbox') ? 
-                        $(this).attr('checked') : 
-                        $(this).val();
-                }
-            });
-        }
-        else {
-            vals[actsOn.name] = $(actsOn).is(':checkbox') ? 
-                $(actsOn).attr('checked') : 
-                $(actsOn).val();
-        }
-        /**
-         * Perform http request and trigger callbacks respectively
-         */
-        // Callback triggered when ajax sending starts
-        options.send ? options.send($(actsOn)) : false;
-        $.ajax({
-            type: options.method,
-            data: vals,
-            url: options.url,
-            dataType: options.dataType,
-            success: function(resp) {
-                options.success ? options.success(resp) : false;
-            },
-            error: function(resp) {
-                options.error ? options.error(resp) : false;
+        // if there is a timer going and we are grouping by interval, wait
+        // for the timer to call notify before saving
+        if (!timer || options.group.byInterval && context === timer) {
+          // if we are grouping by event, wait for that event to fire
+          if (!eventType || options.group.byEvent && context === eventType) {
+            // if we are saving grouped values, use all targets, otherwise
+            // only use targets that have been changed since the last save
+            var $fields = options.group.byChanged
+              ? fields(changed) : fields(targets),
+              $forms = forms($fields);
+
+            // if we have something to update...
+            if ($fields.length && (!condition || condition.call(
+              $elements, options, changed, $forms, $fields) === true)) {
+              // send requests separately if grouping by form
+              if (options.group.byForm) {
+                $forms.each(function(i, form) {
+                  save($fields.filter(function() {
+                    return this.form === form;
+                  }).serializeArray(), targets);
+                });
+              }
+
+              // if we are grouping form field data, it can be sent in one request
+              else save($fields.serializeArray(), targets);
             }
-        });
-    }
-})(jQuery);
+          }
+        }
 
-/**
- * A default (example) of a visualizer you can use that will
- * put a neat loading image in the nearest <legend>
- * for the element/form you were autosaving.
- * Notice: No default "remover" of this spinner exists
- */
-defaultAutosaveSendVisualizer = function(node) {
-    var refNode;
-    if (node.is('form'))
-        refNode = $(node).find('legend');
-    else
-        refNode = $(node).parent('fieldset').find('legend');
-    // Create spinner
-    var spinner = $('<img src="spin.gif" />').css({
-        'position':'relative',
-        'margin-left':'10px',
-        'height': refNode.height(),
-        'width': refNode.height()
+        // nothing to update on this loop or condition failed, continue
+        if (timer && context === timer) loop(targets);
+      },
+      // performs the actual saving of data
+      save = function(data, targets) {
+        console.log("save:", data, targets);
+
+        // reset changed elements list
+        changed = [];
+
+        // if we have a timer interval, continue the loop
+        if (timer) loop(targets);
+      };
+
+    // save references to the forms and fields that were
+    // passed into the plugin
+    var $forms = forms(), $fields = fields();
+
+    // set up form listeners
+    $fields.each(function() {
+      // IE doesn't fire the change event for checkboxes and select multiples
+      var eventType = (this.type == 'checkbox' ||
+        this.tagName.toLowerCase() == "select" && this.multiple)
+        && ("onpropertychange" in document.body) ? "propertychange" : "change";
+
+      $(this).bind(eventType, function(e) {
+        // if type is propertychange, only fire trigger for certain
+        // propertyName's (see regex above)
+        if (e.type == "change" || (e.type == "propertychange"
+          && rprops.test(window.event.propertyName))) {
+          // add this element to the list of changed elements
+          changed.push(this);
+
+          // notify plugin of update and save
+          if (options.save.onUpdate) {
+            console.log("onUpdate:", this);
+            notify(this);
+          }
+        }
+      });
     });
-    spinner.appendTo(refNode);
-}
+
+    // save when an event fires
+    if (options.save.onEvent) {
+      console.log("onEvent:", eventType);
+
+      $forms.bind(eventType, function() {
+        notify(this, eventType);
+      });
+    }
+
+    // attempt to save at regular intervals. actual save will only
+    // happen if form values have changed since the last interval
+    if (options.save.onInterval) {
+      console.log("onInterval:", interval);
+
+      $forms.bind("start." + eventType, function() {
+        loop(this);
+      }).bind("stop." + eventType, function() {
+        loop(false);
+      }).trigger("start." + eventType);
+    }
+
+    return $elements;
+  }
+
+  // Expose default options for plugin override
+  $.fn.autosave.options = {
+    save: {
+      onEvent: false,
+      onChange: true,
+      onInterval: true,
+      onConditoin: false
+    },
+    group: {
+      byForm: true,
+      byEvent: false,
+      byChanged: true,
+      byInterval: true
+    },
+    method: "ajax",
+    methods: {
+      ajax: {
+        url: window.location.href,
+        type: 'POST'
+      }
+    }
+  }
+})(jQuery);
