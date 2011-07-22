732912350e59dd86d9f00f9a1025b96c24f7208b
Kyle Florence <kyle.florence@gmail.com>
2011-07-21 18:45:32 -0700

    Fix bug in AJAX save method if no options.data parameter is passed; update example to show how to use custom options in an AJAX save.

--- src/jquery.autosave.js
+++ src/jquery.autosave.js
@@ -654,6 +654,24 @@
         // Allow for dynamically generated data
         if ($.isFunction(o.data)) {
           o.data = o.data.call(self, formData);
+
+          var formDataType = _type(formData),
+              optionsDataType = _type(o.data);
+
+          // Data types must match in order to merge
+          if (formDataType === optionsDataType) {
+            throw "Type mismatch: cannot merge form data with options data!";
+          } else if (formDataType === "array") {
+            o.data = $.merge(formData, o.data);
+          } else if (formDataType === "object") {
+            o.data = $.extend(formData, o.data);
+          } else if (formDataType === "string") {
+            o.data = formData + (formData.length ? "&" : "") + o.data;
+          }
+
+        // If no user defined data is given, use formData
+        } else if (o.data === undefined) {
+          o.data = formData;
         }
 
         // Wrap the complete method with our own
@@ -664,21 +682,6 @@
 
           self.next("save");
         };
-
-        var formDataType = _type(formData), optionsDataType = _type(o.data);
-
-        // Data types must match in order to merge
-        if ( formDataType !== optionsDataType ) {
-          throw "Type mismatch: cannot merge form data with options data!";
-        } else if ( formDataType === "array" ) {
-          o.data = $.merge(formData, o.data);
-        } else if ( formDataType === "object" ) {
-          o.data = $.extend(formData, o.data);
-        } else if ( formDataType === "string" ) {
-          o.data = formData + (formData.length ? "&" : "") + o.data;
-        } else {
-          o.data = formData;
-        }
 
         $.ajax(o);
 
